5fd7bae518a9d996f78b1141226219cb14263916
磊磊 <39178697+QuHonglei@users.noreply.github.com>
2019-09-17 20:53:46 +0800

    Update Num.java

--- MaxMinSub/src/Num.java
+++ MaxMinSub/src/Num.java
@@ -6,26 +6,34 @@ public class Num {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] a= new int[5];
-		int i,j,max=a[0],min=0,sub=0,b;
+		//int[] a={5,3,6,4,7};
+		int i;
 		Scanner in =new Scanner(System.in);
 		for(i=0;i<5;i++)
 		{
-			
 			a[i]=in.nextInt();
 		}
+		int max=a[0],min=a[0],sub=0,b;
 		for(i=0;i<5;i++)
 		{
-				if(a[i]>max)
+				if(max<a[i])
 				{
 					max=a[i];
+				}
+				if(min>a[i])
+				{
+					min=a[i];
 				}
 		}
 		for(int c:a)
 		{
-			System.out.println(c);
+			System.out.print(c+" ");
 		}
+		System.out.println();
 		sub=max-min;
-		System.out.println("max= "+max+"min= "+min+"sub= "+sub);
+		System.out.println("max="+max);
+		System.out.println("min="+min);
+		System.out.println("sub="+sub);
+		
 	}
-
 }
